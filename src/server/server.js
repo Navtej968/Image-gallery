6520d61cb129c629a66318abd70c50904bd69b41
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-03-22 11:21:10 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 app.use(cors());
 
 
-const filePath = path.join(process.cwd(), "src/server/images.txt");
+const filePath = path.join(process.cwd(), "./images.txt");
 
 
 app.get("/api/images", (req, res) => {
